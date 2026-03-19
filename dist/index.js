60be472931750e0824c45e1fb9bba52b9124be60
Siva <sivatejanandareddy@gmail.com>
2026-03-19 22:47:09 +0530

    built backend of small chat application that uses websockets for RTC

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,28 @@
 import { WebSocketServer, WebSocket } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
-let userCount = 0;
 let allSockets = [];
 wss.on("connection", (socket) => {
-    allSockets.push(socket);
-    userCount = userCount + 1;
-    console.log("user #" + userCount + " Connected");
     socket.on("message", (message) => {
-        console.log("message received " + message.toString());
-        for (let i = 0; i < allSockets.length; i++) {
-            const s = allSockets[i];
-            //@ts-ignore
-            s.send(message.toString() + " :sent from server side");
+        const parsedMessage = JSON.parse(message);
+        if (parsedMessage.type == "join") {
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.roomId
+            });
+        }
+        if (parsedMessage.type == "chat") {
+            //const currentUserRoom = allSockets.find((x)=>x.socket == socket).room;
+            let currentUserRoom = null;
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i]?.socket == socket) {
+                    currentUserRoom = allSockets[i]?.room;
+                }
+            }
+            for (let i = 0; i < allSockets.length; i++) {
+                if (currentUserRoom == allSockets[i]?.room) {
+                    allSockets[i]?.socket.send(parsedMessage.payload.message);
+                }
+            }
         }
     });
 });
