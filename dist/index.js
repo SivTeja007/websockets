bc64253396a66e53924b0a9747922ceb37895d68
Siva <sivatejanandareddy@gmail.com>
2026-03-19 21:37:50 +0530

    Initial project commit

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+import { WebSocketServer, WebSocket } from "ws";
+const wss = new WebSocketServer({ port: 8080 });
+let userCount = 0;
+let allSockets = [];
+wss.on("connection", (socket) => {
+    allSockets.push(socket);
+    userCount = userCount + 1;
+    console.log("user #" + userCount + " Connected");
+    socket.on("message", (message) => {
+        console.log("message received " + message.toString());
+        for (let i = 0; i < allSockets.length; i++) {
+            const s = allSockets[i];
+            //@ts-ignore
+            s.send(message.toString() + " :sent from server side");
+        }
+    });
+});
+//# sourceMappingURL=index.js.map
